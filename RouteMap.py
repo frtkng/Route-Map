1169e6bdc340dd76a7d5a80c7e02c8e1832c9f42
KengoF <frtkng@gmail.com>
2025-03-09 17:42:54 +0900

    プロットに位置IDを付記するように更新

--- RouteMap.py
+++ RouteMap.py
@@ -1,7 +1,7 @@
 import folium
 import pandas as pd
 
-# CSVファイルから緯度経度を読み込み
+# CSVファイルから緯度経度とID番号を読み込み
 data = pd.read_csv('lat_lon_list.csv')
 
 # 最初の座標を基準にマップの中心を設定
@@ -10,9 +10,12 @@ center_lat, center_lon = data.iloc[0]['latitude'], data.iloc[0]['longitude']
 # Folium地図オブジェクトを作成
 m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
 
-# 緯度経度をすべて地図上にプロット
+# 緯度経度とID番号を地図上にプロット
 for _, row in data.iterrows():
-    folium.Marker(location=[row['latitude'], row['longitude']]).add_to(m)
+    folium.Marker(
+        location=[row['latitude'], row['longitude']],
+        popup=f"ID: {row['id']}"
+    ).add_to(m)
 
 # HTMLファイルとして地図を保存
 m.save('location_map.html')
